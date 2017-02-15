a185a18dc353c734ae114732772aa50c244b61af
Aldo Ortiz <aldo_ortiz@migesa.com.mx>
2017-02-15 11:43:25 -0600

    Se modifica el script que cargara las direcciones en la bd y se agregan los sh que los ejecutaran

---
+++ col_agencias.sh
@@ -0,0 +1,2 @@
+cd /var/www/html/mexicanaDes/mexicana/dataLayer/callsWeb
+php WSColAgencia.php
--- dataLayer/callsWeb/WSDirAgencia.php
+++ dataLayer/callsWeb/WSDirAgencia.php
@@ -11,15 +11,21 @@ include_once dirname(dirname(dirname(__FILE__))) . "/dataLayer/DAO.php";
 $DB = new DAO();
 $conn = $DB->getConnect();
 //validamos si el archivo existe en la carpeta
-$archivoCSV = "../../uploads/csvAgencias/direcciones_agencias.csv";
 error_log('message ruta '.getcwd());
+$rutaActual = getcwd();
+chdir('../../uploads/csvAgencias');
+error_log('message ruta '.getcwd());
+$archivoCSV = getcwd()."/direcciones_agencia.csv";
+error_log($archivoCSV);
 if (file_exists($archivoCSV)) {
+    error_log('message existe archivo');
+    chdir($rutaActual);
     $stmtDelFirst="TRUNCATE TABLE direccionesAgenciaCSV";
     if ($delDirAgency = $conn->prepare($stmtDelFirst)) {
         if ($delDirAgency->execute()) {
             error_log("registros eliminados");
             mysqli_query($conn,"SET NAMES 'UTF8'");
-            $sql2 = "LOAD DATA LOCAL INFILE '../../uploads/csvAgencias/direcciones_agencias.csv' INTO TABLE `direccionesAgenciaCSV` FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\\r\\n' (idAgencia, nombreAgencia, idDireccion, calle, numero, entreCalles, idMunicipio, idColonia)";
+            $sql2 = "LOAD DATA LOCAL INFILE '../../uploads/csvAgencias/direcciones_agencia.csv' INTO TABLE `direccionesAgenciaCSV` FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\\r\\n' (idAgencia, nombreAgencia, idDireccion, calle, numero, entreCalles, idMunicipio, idColonia)";
             $resultDump=mysqli_query($conn, $sql2);
             //mysqli_set_local_infile_default($conn);
             if($resultDump){
